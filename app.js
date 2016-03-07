e401be540d8633b6b82afd15bc2a34bc05288f0d
duanlongxian <duanlongxian@szzbmy.com>
2016-03-07 09:12:32 +0800

    auto restart when project file change

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 var mock=require('./src/index');
+var watchr = require('watchr');
+// Watch a directory or file 
+//console.log('Watch our paths');
 mock('mock.json');
+
+watchr.watch({
+	paths: ['./'],
+	listeners: {
+		change: function(changeType,filePath,fileCurrentStat,filePreviousStat){
+			// console.log('a change event occured:',arguments);
+            console.log('a change event occured:',filePath);  
+            console.log('restart mockServer');  
+            
+                    
+		}
+	},
+	next: function(err,watchers){
+		if (err) {
+			return console.log("watching everything failed with error", err);
+		} else {
+			console.log('watching everything completed');
+		}
+	}
+});
